d7efa634f7a1d5ff0e861ec36e6823932d1a983d
Avishek <82322452+avishek-hub@users.noreply.github.com>
2022-11-02 13:07:31 +0530

    Add files via upload

---
+++ Full stack developers road map notes.js
@@ -0,0 +1,16 @@
+/* TIME NEEDED TO LEARN WEB DEVELOPMENT FROM SCRATCH 
+
+FRONT END DEVELOPER
+HTML-   1 Week
+CSS-    1 Month
+JavaScript- 2 Months          (Imp-  DOM Manupulation)
+React.Js-   2 Months          (Resourse- Documentations)
+
+
+BACK END DEVELOPER
+Data Base  Basic-   1 month
+          Ace It-   2 to 3 month
+Processing Language
+
+Project-    Hotel Management 
+            e-Commers Web site
